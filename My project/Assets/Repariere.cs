d135ebeb5d10b84692e2b330f63bb6c6ab35e186
BloodArchVoid <145787598+BloodArchVoid@users.noreply.github.com>
2023-09-23 12:01:15 +0200

    Raumschiff+anfang reparieren

---
+++ My project/Assets/Repariere.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Repariere : MonoBehaviour
+{
+    private bool isPlayerInRange=false;
+    public GameObject Spieler;
+    public int fortschritt;
+    public Text textfield1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlayerInRange&&Input.GetButtonDown("Fire1"))
+        {
+            var Inventar = Spieler.GetComponent<Inventar>();
+            if (Inventar!=null)
+            {
+                Inventar.removeReparaturTeil();
+                fortschritt++;
+            }
+            
+        }
+        textfield1.text=fortschritt.ToString();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Enter");
+        if(col.gameObject.name=="Spieler")
+        {
+            isPlayerInRange=true;
+            Spieler = col.gameObject;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Debug.Log("Exit");
+        if(col.gameObject.name=="Spieler")
+        {
+            isPlayerInRange=false;
+            Spieler = null;
+        }
+    }
+}
