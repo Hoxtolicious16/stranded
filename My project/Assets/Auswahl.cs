2bdd87d9f1dac21917738ca256ce29594639132a
BloodArchVoid <145787598+BloodArchVoid@users.noreply.github.com>
2023-09-23 17:56:51 +0200

    Auswahl

--- My project/Assets/Auswahl.cs
+++ My project/Assets/Auswahl.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Auswahl : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void WechselV()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public void WechselN()
     {
         
     }
