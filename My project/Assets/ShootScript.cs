8e11a77a3726625e956583c988c589b576d5714a
FelixSchade <145787292+FelixSchade@users.noreply.github.com>
2023-09-23 16:42:35 +0200

    BulletNeuer

--- My project/Assets/ShootScript.cs
+++ My project/Assets/ShootScript.cs
@@ -8,6 +8,12 @@ public class ShootScript : MonoBehaviour
 
     Vector2 direction;
 
+    public GameObject Bullet;
+
+    public Transform ShootPoint;
+
+    public float BulletSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +26,21 @@ public class ShootScript : MonoBehaviour
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         direction = mousePos - (Vector2)Gun.position;
         FaceMouse();
+
+        if(Input.getmouseButtonDown(0))
+        {
+            shoot();
+        }
     }
 
     void FaceMouse()
     {
         Gun.transform.right = direction;
     }
+    void shoot()
+    {
+        GameObject BulletIns = Instantiate(Bullet,ShootPoint.position,shootPoint.rotation);
+    BulletIns.GetComponent<Ridgidbody2D>().AddForce(BulletIns.transform.right * BulletSpeed);
+    }
 }
+
