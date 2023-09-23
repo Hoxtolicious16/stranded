9bce3fb92770e8853d68f2b60c418116cc8a07c0
FelixSchade <145787292+FelixSchade@users.noreply.github.com>
2023-09-23 12:11:39 +0200

    Assets importiert

---
+++ My project/Assets/Scripts/Waffe.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waffe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Console.KeyAvailable)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+
+            if (keyInfo.Key == ConsoleKey.Spacebar)
+            {
+                Console.WriteLine("Spacebar pressed")
+                        }
+        }
+    }
+}
