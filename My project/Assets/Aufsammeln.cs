db0c33b5dc9086e86988c70111e19b9475df6d86
TaseFase <145788797+TaseFase@users.noreply.github.com>
2023-09-24 12:22:47 +0200

    aufsammeln

---
+++ My project/Assets/Aufsammeln.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class Aufsammeln : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject ReparaturTeil;
+    private bool isPlayerInRange=false;
+    GameObject rT= null;//Fehler
+    void Start()
+    public GameObject Spieler;
+    void Update()
+    {
+        if(isPlayerInRange&&Input.GetButtonDown("Fire1"))
+        {
+            rT=gameObject; //Fehler
+            Destroy(gameObject);
+            var Inventar = Spieler.GetComponent<Inventar>();
+            if (Inventar!=null)
+            {
+                Inventar.addReparaturTeil();
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+
+        if(col.gameObject.name=="Spieler")
+        {
+            isPlayerInRange=true;
+            Spieler = col.gameObject;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+        if(col.gameObject.name=="Spieler")
+        {
+            isPlayerInRange=false;
+            Spieler = null;
+        }
+    }
+}
