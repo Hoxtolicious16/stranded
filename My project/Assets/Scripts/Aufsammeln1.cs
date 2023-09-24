a87af4e5b4955a2e21506ba571d3b292692f5f89
TaseFase <145788797+TaseFase@users.noreply.github.com>
2023-09-24 11:47:29 +0200

    AddBeere

--- My project/Assets/Scripts/Aufsammeln1.cs
+++ My project/Assets/Scripts/Aufsammeln1.cs
@@ -2,56 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Aufsammeln : MonoBehaviour
+public class Aufsammeln1 : MonoBehaviour
 {
-    private bool isPlayerInRange = false;
-    private int beerenGesammelt = 0; // Anzahl der gesammelten Beeren
-    public int maxBeeren = 3; // Maximale Anzahl der aufsammelbaren Beeren
-    public GameObject Spieler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        if (isPlayerInRange && Input.GetButtonDown("Fire1") && beerenGesammelt < maxBeeren)
-        {
-            var Inventar = Spieler.GetComponent<Inventar>();
-            if (Inventar != null)
-            {
-                Inventar.addBeere();
-                beerenGesammelt++;
-
-                if (beerenGesammelt >= maxBeeren)
-                {
-                    Debug.Log("Maximale Anzahl an Beeren erreicht: " + maxBeeren);
-                    // Weitere Aktionen, wenn die maximale Anzahl erreicht wurde.
-                }
-            }
-        }
-
-        // Wenn "R" gedrückt wird und die maximale Anzahl Beeren erreicht ist, setze die Variable auf drei.
-        if (Input.GetKeyDown(KeyCode.R) && beerenGesammelt == maxBeeren)
-        {
-            Debug.Log("Variable auf " + maxBeeren + " setzen.");
-            beerenGesammelt = maxBeeren;
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        Debug.Log("Enter");
-        if (col.gameObject.name == "Spieler")
-        {
-            isPlayerInRange = true;
-            Spieler = col.gameObject;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D col)
-    {
-        Debug.Log("Exit");
-        if (col.gameObject.name == "Spieler")
-        {
-            isPlayerInRange = false;
-            Spieler = null;
-        }
+        
     }
 }
